ce47476bda12903b863be386fed6a487a3b81b10
odyssic <stevenbuddenjr@gmail.com>
2020-01-31 13:35:54 -0500

    I dunno a few more lines of code, less functionality

--- script.js
+++ script.js
@@ -1,65 +1,46 @@
-// submit button examples
+// debilitate submit button
 
-// let el = document.querySelector(".example-form");
+let parkingForm = document.querySelector("#parking-form");
 
-// el.addEventListener("submit", function(e) {
-//   e.preventDefault();
-//   console.log(e);
-//   console.log(e.target);
-// });
+parkingForm.addEventListener("submit", function() {
+    e.preventDefault();
+});
 
+// run program on mouseclick on submit button; no run on submit
+
+const submitButton = document.querySelector("#submit-button");
+submitButton.addEventListener("onclick", nameValidate());
 
 function nameValidate() {
     const inputBoxes = document.querySelectorAll("input");
-    for (let box of inputBoxes) {
-        parent = box.parentElement;
-        if (box.value.length >= 2) {
 
-            if (!parent.classList.contains('input-valid')) {
-                parent.classList.add('input-valid')
+    for (let inputBox of inputBoxes) {
+        parent = inputBox.parentElement;
+        // if (box.value.length = 0) {
+        //     return false;
+        if (inputBox.value.length > 0) {
+            if (!parent.classList.contains("input-valid")) {
+                parent.classList.add("input-valid");
+                inputBox.value = "";
             }
         } else {
-            if (!parent.classList.contains('input-invalid')) {
-                parent.classList.add('input-invalid')
-            }
-
-
+            !parent.classList.contains("input-invalid");
+            parent.classList.add("input-invalid");
+            inputBox.value = "Please enter a valid ";
         }
     }
 }
 
-nameValidate()
+function validateEmail() {
+    let emailInput = document.querySelector("#email-input");
+    let emailAddress = emailInput.value;
+    let parentDiv = emailInput.parentNode;
 
-// add listener 
-// div class = "input-field input-valid" >
-//     <
-//     label > Valid < /label> <
-// input type = "text" >
-//     <
-//     /div>
-
-// <
-// div class = "input-field input-invalid" >
-//     <
-//     label > Invalid < /label> <
-// input type = "text" >
-//     <
-//     /div>
-
-// validate various fields. what can we validate by group? Above could probably apply to all fields. If empty
-
-// <
-// div class = "input-field input-valid" >
-//     <
-//     label > Valid < /label> <
-// input type = "text" >
-//     <
-//     /div>
-
-// <
-// div class = "input-field input-invalid" >
-//     <
-//     label > Invalid < /label> <
-// input type = "text" >
-//     <
-//     /div>
+    if (emailAddress !== "") {
+        parentDiv.classList.remove("input-invalid");
+        parentDiv.classList.add("input-valid");
+    } else {
+        parentDiv.classList.remove("input-valid");
+        parentDiv.classList.add("input-invalid");
+    }
+}
