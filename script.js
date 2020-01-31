15b407995b614a383b268e1cdc9be8c044362d99
odyssic <stevenbuddenjr@gmail.com>
2020-01-31 14:59:34 -0500

    this is a file doing little

--- script.js
+++ script.js
@@ -2,45 +2,74 @@
 
 let parkingForm = document.querySelector("#parking-form");
 
-parkingForm.addEventListener("submit", function() {
+parkingForm.addEventListener("submit", function(e) {
     e.preventDefault();
+    nameValidate();
+
 });
+
+// document.querySelector("submit-button").disabled = true;
 
 // run program on mouseclick on submit button; no run on submit
 
-const submitButton = document.querySelector("#submit-button");
-submitButton.addEventListener("onclick", nameValidate());
+//check for string and convert to number? Beause all inputs will be strings? 
 
 function nameValidate() {
-    const inputBoxes = document.querySelectorAll("input");
-
-    for (let inputBox of inputBoxes) {
-        parent = inputBox.parentElement;
-        // if (box.value.length = 0) {
-        //     return false;
-        if (inputBox.value.length > 0) {
-            if (!parent.classList.contains("input-valid")) {
-                parent.classList.add("input-valid");
-                inputBox.value = "";
-            }
-        } else {
-            !parent.classList.contains("input-invalid");
-            parent.classList.add("input-invalid");
-            inputBox.value = "Please enter a valid ";
-        }
+    const name = document.querySelector("name");
+    if (name.value.length > 0) && (name.value == String) {
+        parent.classList.add("input-valid");
+        inputBox.value = "";
     }
+} else {
+    parent.classList.add("input-invalid");
+    inputBox.value = "'Name' is required!";
+}
 }
 
-function validateEmail() {
-    let emailInput = document.querySelector("#email-input");
-    let emailAddress = emailInput.value;
-    let parentDiv = emailInput.parentNode;
+function carModelValidate() {
 
-    if (emailAddress !== "") {
-        parentDiv.classList.remove("input-invalid");
-        parentDiv.classList.add("input-valid");
-    } else {
-        parentDiv.classList.remove("input-valid");
-        parentDiv.classList.add("input-invalid");
-    }
+
 }
+
+function validateDate() {
+    let parkingDate = document.querySelector("start-date");
+    if (parkingDate = instanceof date
+    }
+
+    //invalidates or validates ALL based on input of any kind and length
+
+    // function nameValidate() {
+    //     const inputBoxes = document.querySelector("input");
+
+    //     for (let inputBox of inputBoxes) {
+    //         parent = inputBox.parentElement;
+    //         // if (box.value.length = 0) {
+    //         //     return false;
+    //         if (inputBox.value.length > 0) {
+    //             if (!parent.classList.contains("input-valid")) {
+    //                 parent.classList.add("input-valid");
+    //                 inputBox.value = "";
+    //             }
+    //         } else {
+    //             !parent.classList.contains("input-invalid");
+    //             parent.classList.add("input-invalid");
+    //             inputBox.value = "Please enter a valid ";
+    //         }
+    //     }
+    // }
+
+    //Hypothetical (THERE IS NO EMAIL VALIDATE)
+
+    // function validateEmail() {
+    //     let emailInput = document.querySelector("#email-input");
+    //     let emailAddress = emailInput.value;
+    //     let parentDiv = emailInput.parentNode;
+
+    //     if (emailAddress !== "" && emailAddress.contains("@")) {
+    //         parentDiv.classList.remove("input-invalid");
+    //         parentDiv.classList.add("input-valid");
+    //     } else {
+    //         parentDiv.classList.remove("input-valid");
+    //         parentDiv.classList.add("input-invalid");
+    //     }
+    // }
