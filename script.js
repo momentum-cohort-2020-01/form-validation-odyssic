dbcbc0b6eeb9fb184999b84551891f28831b24f5
odyssic <stevenbuddenjr@gmail.com>
2020-02-01 13:45:09 -0500

    updates more functions

--- script.js
+++ script.js
@@ -2,74 +2,86 @@
 
 let parkingForm = document.querySelector("#parking-form");
 
-parkingForm.addEventListener("submit", function(e) {
-    e.preventDefault();
+parkingForm.addEventListener("submit", function(event) {
+    event.preventDefault();
     nameValidate();
-
+    // carModelValidate();
+    // function getTotal(){}
+    //add all functions
 });
+// variables for whole document
 
-// document.querySelector("submit-button").disabled = true;
-
-// run program on mouseclick on submit button; no run on submit
-
-//check for string and convert to number? Beause all inputs will be strings? 
+const letters = /^[A-Za-z]+$/;
+const numbers = /^[0-9]+$/;
 
 function nameValidate() {
-    const name = document.querySelector("name");
-    if (name.value.length > 0) && (name.value == String) {
-        parent.classList.add("input-valid");
-        inputBox.value = "";
+    const wholeName = document.querySelector("#name");
+
+    let myNewDiv = wholeName.before("Valid name");
+    let upperCaseName = wholeName.value.toUppercase();
+
+    if (wholeName.value.length > 1) {
+        wholeName.parentElement.classList.add("input-valid");
+        return upperCaseName;
+        // && (wholeName.value.match(letters))
+    } else {
+        wholeName.parentElement.classList.add("input-invalid");
+        myNewDiv.innerHTML = "*Valid Name Required";
+        alert("Please enter a valid name!");
     }
-} else {
-    parent.classList.add("input-invalid");
-    inputBox.value = "'Name' is required!";
-}
 }
 
-function carModelValidate() {
+// function carYearValidate() {
+//     const carYear = document.querySelector("#car-year")
+//     if ((carYear.value != "") && ()) {
 
+//     }
 
-}
+//     carMakeValidate()
 
-function validateDate() {
-    let parkingDate = document.querySelector("start-date");
-    if (parkingDate = instanceof date
-    }
+//     carModelValidate()
 
-    //invalidates or validates ALL based on input of any kind and length
+// }
 
-    // function nameValidate() {
-    //     const inputBoxes = document.querySelector("input");
+// function validateParkingDate() {
+//     let parkingDate = document.querySelector("#start-date");
+//     if (parkingDate = date
+//     }
 
-    //     for (let inputBox of inputBoxes) {
-    //         parent = inputBox.parentElement;
-    //         // if (box.value.length = 0) {
-    //         //     return false;
-    //         if (inputBox.value.length > 0) {
-    //             if (!parent.classList.contains("input-valid")) {
-    //                 parent.classList.add("input-valid");
-    //                 inputBox.value = "";
-    //             }
-    //         } else {
-    //             !parent.classList.contains("input-invalid");
-    //             parent.classList.add("input-invalid");
-    //             inputBox.value = "Please enter a valid ";
-    //         }
-    //     }
-    // }
+//invalidates or validates ALL based on input of any kind and length
 
-    //Hypothetical (THERE IS NO EMAIL VALIDATE)
+// function nameValidate() {
+//     const inputBoxes = document.querySelector("input");
 
-    // function validateEmail() {
-    //     let emailInput = document.querySelector("#email-input");
-    //     let emailAddress = emailInput.value;
-    //     let parentDiv = emailInput.parentNode;
+//     for (let inputBox of inputBoxes) {
+//         parent = inputBox.parentElement;
+//         // if (box.value.length = 0) {
+//         //     return false;
+//         if (inputBox.value.length > 0) {
+//             if (!parent.classList.contains("input-valid")) {
+//                 parent.classList.add("input-valid");
+//                 inputBox.value = "";
+//             }
+//         } else {
+//             !parent.classList.contains("input-invalid");
+//             parent.classList.add("input-invalid");
+//             inputBox.value = "Please enter a valid ";
+//         }
+//     }
+// }
 
-    //     if (emailAddress !== "" && emailAddress.contains("@")) {
-    //         parentDiv.classList.remove("input-invalid");
-    //         parentDiv.classList.add("input-valid");
-    //     } else {
-    //         parentDiv.classList.remove("input-valid");
-    //         parentDiv.classList.add("input-invalid");
-    //     }
-    // }
+//Hypothetical (THERE IS NO EMAIL VALIDATE)
+
+// function validateEmail() {
+//     let emailInput = document.querySelector("#email-input");
+//     let emailAddress = emailInput.value;
+//     let parentDiv = emailInput.parentNode;
+
+//     if (emailAddress !== "" && emailAddress.contains("@")) {
+//         parentDiv.classList.remove("input-invalid");
+//         parentDiv.classList.add("input-valid");
+//     } else {
+//         parentDiv.classList.remove("input-valid");
+//         parentDiv.classList.add("input-invalid");
+//     }
+// }
